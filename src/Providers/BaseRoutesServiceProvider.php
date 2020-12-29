ad6f2f8b8dd1f11c45fd7567f3d8f0b1645e45db
artem <almatyartem@mail.ru>
2020-12-29 09:55:30 +0200

    add BaseRoutesServiceProvider

---
+++ src/Providers/BaseRoutesServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace LaravelSferaLibrary\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+use LaravelSferaLibrary\Http\RouteData;
+
+abstract class BaseRoutesServiceProvider extends RouteServiceProvider
+{
+    /**
+     * @var RouteData[]
+     */
+    protected array $routesData;
+
+    public function boot()
+    {
+        $this->setRoutesData();
+        $this->map();
+
+        parent::boot();
+    }
+
+    /**
+     * @return string
+     */
+    abstract function getModuleName() : string;
+
+    /**
+     * @return int
+     */
+    abstract function getVersion() : int;
+
+    /**
+     *
+     */
+    abstract function setRoutesData() : void;
+
+    /**
+     * @return mixed
+     */
+    function map()
+    {
+        Route::prefix('api/'.$this->getModuleName().'/v'.$this->getVersion())
+            ->group(function(Router $router){
+                foreach($this->routesData as $routeData){
+                    $method = $routeData->httpMethod;
+                    $router->$method($routeData->uri, [$routeData->controllerClass, $routeData->methodName]);
+                }
+            });
+    }
+}
