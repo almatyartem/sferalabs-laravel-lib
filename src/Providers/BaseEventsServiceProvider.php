eb3186093aca4127c25c873bb5f1e0e7ee706aea
artem <almatyartem@mail.ru>
2021-01-17 03:15:02 +0200

    fix

---
+++ src/Providers/BaseEventsServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaravelSferaLibrary\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider;
+
+abstract class BaseEventsServiceProvider extends EventServiceProvider
+{
+
+}
